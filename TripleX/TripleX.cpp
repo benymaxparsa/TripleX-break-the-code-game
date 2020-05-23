7a3bd714b2bb0fd837d63aeae09e570e24af5def
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-05-23 10:21:54 +0430

    video part 9

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -5,5 +5,15 @@ int main()
 	std::cout << "You're a hacker trying to breaking into the BadGuys corp servers in order to access their Data Bases" << std::endl;
 	std::cout << "You need to find their passwords to continue..." << std::endl;
 
+	const int a = 4;
+	const int b = 3;
+	const int c = 2;
+
+	const int sum = a + b + c;
+	const int product = a * b * c;
+
+	std::cout << sum << std::endl;
+	std::cout << product << std::endl;
+
 	return 0;
 }
