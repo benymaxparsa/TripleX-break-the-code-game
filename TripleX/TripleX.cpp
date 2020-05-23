a0c1d4a573b6fabaf1567a2b7a32777fe4bad4f6
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-05-23 09:57:19 +0430

    first step

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -2,6 +2,8 @@
 
 int main() 
 {
-	
+	std::cout << "You're a hacker trying to breaking into the BadGuys corp servers in order to access their Data Bases" << std::endl;
+	std::cout << "You need to find their passwords to continue..." << std::endl;
+
 	return 0;
 }
